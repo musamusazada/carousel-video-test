ed855f1bf09968a04ab95b9f99b416367a5c4de0
Musa Musazada <musaam@code.edu.az>
2021-11-09 21:48:56 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+ $('.owl-carousel').owlCarousel({
+    items: 1,
+        loop:false,
+        margin:0,
+        nav:0,
+        stagePadding:0,
+        video: true,
+        lazyLoad:true
+       
+
+    })
+
